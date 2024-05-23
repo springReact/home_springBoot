aa3c4b56db0e242a120704b93305d973614c26d2
dev-kang <kingaser@naver.com>
2024-05-23 22:05:51 +0900

    LocalDateFormatter, register

--- src/main/java/org/zerock/apiserver/controller/TodoController.java
+++ src/main/java/org/zerock/apiserver/controller/TodoController.java
@@ -2,14 +2,13 @@ package org.zerock.apiserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.apiserver.dto.PageRequestDTO;
 import org.zerock.apiserver.dto.PageResponseDTO;
 import org.zerock.apiserver.dto.TodoDTO;
 import org.zerock.apiserver.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +28,14 @@ public class TodoController {
         log.info("list................." + pageRequestDTO);
 
         return todoService.getList(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("todoDTO: " + todoDTO);
+
+        Long tno = todoService.register(todoDTO);
+
+        return Map.of("TNO", tno);
     }
 }
